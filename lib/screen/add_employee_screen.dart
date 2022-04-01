77539b6d845897836672062b47f81349dae8a102
Manfriny <manfriny@gmail.com>
2022-04-01 09:46:02 -0300

    add date picker

--- lib/screen/add_employee_screen.dart
+++ lib/screen/add_employee_screen.dart
@@ -1,4 +1,6 @@
+import 'package:employee_book/widget/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AddEmployeeScreen extends StatefulWidget {
   const AddEmployeeScreen({Key? key}) : super(key: key);
@@ -8,7 +10,11 @@ class AddEmployeeScreen extends StatefulWidget {
 }
 
 class _AddEmployeeScreenState extends State<AddEmployeeScreen> {
-  final TextEditingController _controller = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _dateOfBirthController = TextEditingController();
+  DateTime? _dateOfBirth;
 
   @override
   Widget build(BuildContext context) {
@@ -29,23 +35,67 @@ class _AddEmployeeScreenState extends State<AddEmployeeScreen> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
+            CustomTextFormField(
+                controller: _userNameController, txtLabel: 'User Name'),
+            const SizedBox(height: 8.0),
+            CustomTextFormField(
+                controller: _firstNameController, txtLabel: 'First Name'),
+            const SizedBox(height: 8.0),
+            CustomTextFormField(
+                controller: _lastNameController, txtLabel: 'Last Name'),
+            const SizedBox(height: 8.0),
+            //Date Picker Form
             TextFormField(
-              controller: _controller,
+              controller: _dateOfBirthController,
               keyboardType: TextInputType.name,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
-                label: Text('Use Name'),
+                label: Text('Date of Birth'),
               ),
               validator: (value) {
                 if (value == null || value.isEmpty) {
-                  return 'User name cannot be empty';
+                  return 'Date of Birth cannot be empty';
                 }
                 return null;
               },
+              onTap: () => pickDateOfBirth(context),
             ),
+            // END DATE PICKER FORM
+            const SizedBox(height: 8.0),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> pickDateOfBirth(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final newDate = await showDatePicker(
+      context: context,
+      initialDate: _dateOfBirth ?? initialDate,
+      firstDate: DateTime(DateTime.now().year - 100),
+      lastDate: DateTime(DateTime.now().year + 1),
+      builder: (context, child) => Theme(
+        data: ThemeData().copyWith(
+          colorScheme: const ColorScheme.light(
+            primary: Colors.pink,
+            onPrimary: Colors.white,
+            onSurface: Colors.black,
+          ),
+          dialogBackgroundColor: Colors.white,
+        ),
+        child: child ?? const Text(''),
+      ),
+    );
+
+    if (newDate == null) {
+      return;
+    }
+
+    setState(() {
+      _dateOfBirth = newDate;
+      String dob = DateFormat('dd/MM/yyyy').format(newDate);
+      _dateOfBirthController.text = dob;
+    });
   }
 }
