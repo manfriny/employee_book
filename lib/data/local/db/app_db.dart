303a94c9fb583168e6734456009c60236cc86781
Manfriny <manfriny@gmail.com>
2022-04-06 16:32:15 -0300

    aula09 com problema

---
+++ lib/data/local/db/app_db.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:employee_book/data/local/entity/employee_entity.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+
+part 'app_db.g.dart';
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(path.join(dbFolder.path, 'employee.sqlite'));
+
+    return NativeDatabase(file);
+  });
+}
+
+@DriftDatabase(tables: [Employee])
+class AppDb extends $_AppDb {
+  AppDb() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
