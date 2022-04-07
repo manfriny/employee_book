4f063ee45a9b257638e4da7f715f621581048102
Manfriny <manfriny@gmail.com>
2022-04-07 08:25:59 -0300

    aula09 finalizada

--- lib/data/local/db/app_db.dart
+++ lib/data/local/db/app_db.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:employee_book/data/local/entity/employee_entity.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
+
+part 'app_db.g.dart';
 
 LazyDatabase _opeConnection() {
   return LazyDatabase(() async {
@@ -14,4 +17,10 @@ LazyDatabase _opeConnection() {
   });
 }
 
-class AppDb {}
+@DriftDatabase(tables: [Employee])
+class AppDb extends _$AppDb {
+  AppDb() : super(_opeConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
