1fe5dc599f621f399a8dfba6e1cb96dd6291f825
Manfriny <manfriny@gmail.com>
2022-04-07 08:02:57 -0300

    aula09 4min ok

---
+++ lib/data/local/db/app_db.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+
+LazyDatabase _opeConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(path.join(dbFolder.path, 'employee.sqlite'));
+
+    return NativeDatabase(file);
+  });
+}
+
+class AppDb {}
